70dc1ece908f2e228394269988305a684487e356
Felix Bouchard <felixbouc@gmail.com>
2017-03-03 12:19:48 -0500

    created mp3 class and added some items to form

---
+++ visualizer/mp3_data.cpp
@@ -0,0 +1,6 @@
+#include "mp3_data.h"
+
+mp3_data::mp3_data()
+{
+
+}
---
+++ visualizer/mp3_data.h
@@ -0,0 +1,11 @@
+#ifndef MP3_DATA_H
+#define MP3_DATA_H
+
+
+class mp3_data
+{
+public:
+    mp3_data();
+};
+
+#endif // MP3_DATA_H
